2a225966a31397913320316e5248adbdd1bcddbc
jduckling1024 <jduck1024@naver.com>
2021-11-30 20:53:33 +0900

    Feat : Update app.js
    초기 Test용

--- src/app.js
+++ src/app.js
@@ -6,8 +6,9 @@ dotenv.config({
 import express from "express";
 
 const app = express();
+const port = process.env.PORT;
 
-app.listen(8080, function () {
+app.listen(port, () => {
   console.log("listening on 8080");
 });
 
